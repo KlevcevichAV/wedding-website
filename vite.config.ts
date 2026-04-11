39666c9830bbbf4b91e133a20eb585eb456d63c4
Kliautsevich <alexandr.klevcevich@gmail.com>
2026-04-11 18:15:26 +0300

    Add path alias configuration to Vite and update comments

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  // Замени 'wedding-website' на имя твоего репозитория на GitHub
-  base: '/wedding-website/'
+  // ВАЖНО: имя твоего репозитория со слешами
+  base: '/wedding-website/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
